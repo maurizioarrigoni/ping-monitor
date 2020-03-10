54f07fc5447e4bd08844d99c1b7ea72a6951e939
Maurizio Arrigoni <maurizio.arrigoni@tiscali.it>
2020-03-10 10:55:00 +0100

    Add files via upload

---
+++ ping-monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# chmod +x ping-monitor.sh (prima di poter eseguire lo script)
+# apt-get install moreutils (per poter eseguire il comando 'ts')
+# l'orario e' in formato GMT (quindi 1 ora indietro)
+# per ovviare al problema settare la variabile TZ (export TZ=CEST-1)
+
+while true; do
+        ping -c 100 8.8.8.8 |
+        grep -A 3 "statistic" |
+	tail -n +2 | head -n +1 |
+	ts '%Y-%m-%d %H:%M:%S ,' |
+	sed 's/ packets transmitted//' |
+	sed '/errors/! s/ received/, /' |
+	sed '/errors/ s/ received//' |
+	sed 's/ errors//' |
+	sed 's/ packet loss//' |
+	sed 's/time //' |
+	tee -a ping-stats
+done
+
+
