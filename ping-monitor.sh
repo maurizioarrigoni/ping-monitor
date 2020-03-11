9587eccf82d5071adb7581ab26705dbd8b642e46
maurizioarrigoni <maurizio.arrigoni@tiscali.it>
2020-03-11 09:05:37 +0100

    Update ping-monitor.sh

--- ping-monitor.sh
+++ ping-monitor.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 # chmod +x ping-monitor.sh (prima di poter eseguire lo script)
 # apt-get install moreutils (per poter eseguire il comando 'ts')
-# l'orario e' in formato GMT (quindi 1 ora indietro)
-# per ovviare al problema settare la variabile TZ (export TZ=CEST-1)
 
-MY_PATH="`dirname \"$0\"`"              # relative
-MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+ScriptPath="`dirname \"$0\"`"              # relative
+ScriptPath="`( cd \"$ScriptPath\" && pwd )`"  # absolutized and normalized
 
-while true; do
-        ping -c 100 8.8.8.8 |
-        grep -A 3 "statistic" |
-	tail -n +2 | head -n +1 |
-	ts '%Y-%m-%d %H:%M:%S ,' |
-	sed 's/ packets transmitted//' |
-	sed '/errors/! s/ received/, /' |
-	sed '/errors/ s/ received//' |
-	sed 's/ errors//' |
-	sed 's/ packet loss//' |
-	sed 's/time //' |
-	tee -a $MY_PATH/ping-stats
-done
+if [ "$1" ]; then
+  TargetHost="$1"
+
+  while true; do
+    ping -c 100 $TargetHost |
+    grep -A 3 "statistic" |
+    tail -n +2 | head -n +1 |
+    ts '%Y-%m-%d %H:%M:%S ,' |
+    sed 's/ packets transmitted//' |
+    sed '/errors/! s/ received/, /' |
+    sed '/errors/ s/ received//' |
+    sed 's/ errors//' |
+    sed 's/ packet loss//' |
+    sed 's/time //' |
+    tee -a $ScriptPath/ping-stats-$TargetHost
+  done
+else
+  # no target_host parameter
+  echo "Syntax error: $0 {target_host}"
+fi
+
 
 
